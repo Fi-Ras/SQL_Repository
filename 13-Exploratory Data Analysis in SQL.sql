06f22c36ff63fc4817a30816cee4ef87a5d82d13
Firas BAHRI <pro.bahri.firas@gmail.com>
2023-12-12 21:59:18 +0100

    What's in the database?

--- 13-Exploratory Data Analysis in SQL.sql
+++ 13-Exploratory Data Analysis in SQL.sql
@@ -235,3 +235,130 @@ COPY fortune500
 	FROM PROGRAM 'curl "https://assets.datacamp.com/production/repositories/3567/datasets/19cf8e7841e26d71feb3516c7a4b135aff8a8b4f/fortune.csv"' (DELIMITER ',', FORMAT CSV, HEADER, NULL 'NA');
 
 
+-- Select the count of the number of rows
+SELECT count(*)
+  FROM fortune500;
+
+
+-- Select the count of ticker, 
+-- subtract from the total number of rows, 
+-- and alias as missing
+SELECT count(*) - count(ticker) AS missing
+  FROM fortune500;
+
+
+-- Select the count of profits_change, 
+-- subtract from total number of rows, and alias as missing
+SELECT count(*) - count(profits_change) AS missing
+  FROM fortune500;
+
+
+-- Select the count of industry, 
+-- subtract from total number of rows, and alias as missing
+SELECT count(*) - count(industry) AS missing
+  FROM fortune500;
+
+-- Select company names
+SELECT company.name
+  FROM company
+      INNER JOIN fortune500
+      ON company.ticker = fortune500.ticker;
+
+
+-- Count the number of tags with each type
+SELECT type, count(*) AS count
+  FROM tag_type
+ -- To get the count for each type, what do you need to do?
+GROUP BY type
+ -- Order the results with the most common
+ -- tag types listed first
+ORDER BY count DESC;
+
+
+
+-- Select the 3 columns desired
+SELECT company.name, tag_type.tag, tag_type.type
+  FROM company
+  	   -- Join to the tag_company table
+       INNER JOIN tag_company 
+       ON company.id = tag_company.company_id
+       -- Join to the tag_type table
+       INNER JOIN tag_type
+       ON tag_company.tag = tag_type.tag
+  -- Filter to most common type
+  WHERE type='cloud';
+
+
+-- Use coalesce
+SELECT coalesce(industry, sector, 'Unknown') AS industry2,
+       -- Don't forget to count!
+       count(*)
+  FROM fortune500 
+-- Group by what? (What are you counting by?)
+ GROUP BY industry2
+-- Order results to see most common first
+ ORDER by count DESC 
+-- Limit results to get just the one value you want
+ Limit 1;
+
+
+SELECT company_original.name, title, rank
+  -- Start with original company information
+  FROM company AS company_original
+       -- Join to another copy of company with parent
+       -- company information
+	   LEFT JOIN company AS company_parent
+       ON company_original.parent_id = company_parent.id 
+       -- Join to fortune500, only keep rows that match
+       INNER JOIN fortune500 
+       -- Use parent ticker if there is one, 
+       -- otherwise original ticker
+       ON coalesce(company_parent.ticker, 
+                   company_original.ticker) = 
+             fortune500.ticker
+ -- For clarity, order by rank
+ ORDER BY rank; 
+
+
+-- Select the original value
+SELECT profits_change, 
+	   -- Cast profits_change
+       CAST(profits_change AS integer) AS profits_change_int
+  FROM fortune500;
+
+
+-- Divide 10 by 3
+SELECT 10/3 AS "10/3", 
+       -- Cast 10 as numeric and divide by 3
+       10::numeric/3 AS "10/3 To Numeric";
+
+
+SELECT '3.2'::numeric,
+       '-123'::numeric,
+       '1e3'::numeric,
+       '1e-3'::numeric,
+       '02314'::numeric,
+       '0002'::numeric;
+
+
+-- Select the count of each value of revenues_change
+SELECT revenues_change, count(revenues_change)
+  FROM fortune500
+ GROUP BY revenues_change
+ -- order by the values of revenues_change
+ ORDER BY count desc;
+
+
+ -- Select the count of each revenues_change integer value
+SELECT revenues_change::integer, count(revenues_change)
+  FROM fortune500
+ GROUP BY revenues_change::integer
+ -- order by the values of revenues_change
+ ORDER BY count desc;
+
+
+-- Count rows 
+SELECT count(*)
+  FROM fortune500
+ -- Where...
+ WHERE revenues_change >  0;
